949f530a57f4fa6a0fef94a1168bb84b00da0f6e
RIZWAN <morizwan1410@gmail.com>
2025-10-30 21:11:32 +0530

    Player Movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 5f;
     Rigidbody rb;
+    private GameObject focalPoint;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        rb.AddForce(Vector3.forward * forwardInput * speed);
+        rb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
 }
