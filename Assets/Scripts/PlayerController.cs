7bbaeda3a9b658fc10b37b251f6c2e1e976381df
RIZWAN <morizwan1410@gmail.com>
2025-11-05 21:06:41 +0530

    enemy spawn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5f;
+    public float speed = 5f;
     public bool hasPowerup;
     private float powerupForce = 15f;
 
