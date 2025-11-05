2766e960e76619244102f2b6f562200d3f0d39db
RIZWAN <morizwan1410@gmail.com>
2025-11-05 21:03:14 +0530

    enemy spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,7 +9,15 @@ public class SpawnManager : MonoBehaviour
 
     private void Start()
     {
-        Instantiate(enemyPrefab, GenerteRandomPos(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave();        
+    }
+
+    void SpawnEnemyWave()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(enemyPrefab, GenerteRandomPos(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerteRandomPos()
